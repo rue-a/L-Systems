3abb86e54b9e5332d1e6e6655e195d9b897963be
49lf <ircsurfer33@gmail.com>
2025-11-23 14:12:03 -0500

    Add scale property to L-system parameters

--- js/State.js
+++ js/State.js
@@ -1,14 +1,17 @@
 class State {
-    constructor(point, angle) {
+    constructor(point, angle, scale) {
         this.point = point;
         this.angle = angle;
+        this.scale = scale;
     }
 
     get_point() { return (this.point); }
     get_angle() { return (this.angle); }
+    get_scale() { return (this.scale); }
 
     set_point(point) { this.point = point; }
-    set_angel(angle) { this.angle = angle; }
+    set_angle(angle) { this.angle = angle; }
+    set_scale(scale) { this.scale = scale; }
 
-    clone() { return new State(this.point, this.angle); }
+    clone() { return new State(this.point, this.angle, this.scale); }
 }
